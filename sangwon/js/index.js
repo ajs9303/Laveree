5af78de5b33cf0a784447098f5cb687d683c9c5e
tkddnjs0630 <tkddnjs063089@gmail.com>
2025-09-30 22:27:31 +0900

    sangwon

--- sangwon/js/index.js
+++ sangwon/js/index.js
@@ -1,17 +1,38 @@
+const title = document.querySelectorAll(".hiddentitle");
+const BRAND = document.querySelector("#BRAND");
+const SHOP = document.querySelector("#SHOP");
+const MAGAZINE = document.querySelector("#MAGAZINE");
+const COMMUNITY = document.querySelector("#COMMUNITY");
+const STORE = document.querySelector("#STORE");
+
+title.forEach((title) => {
+  BRAND.addEventListener("mouseover", () => {
+    title.classList.remove("hiddentitle");
+  });
+});
+
+// BRAND.addEventListener("mouseover", () => {
+//   BRAND.classList.remove("hiddentitle");
+//   SHOP.classList.remove("hiddentitle");
+//   BRAND.classList.remove("hiddentitle");
+//   BRAND.classList.remove("hiddentitle");
+//   BRAND.classList.remove("hiddentitle");
+// });
+
 // hero
-var swiperA = new Swiper(".swiperA", {
+const swiperA = new Swiper(".swiperA", {
   spaceBetween: 0,
   pagination: {
     el: ".swiper-pagination-A",
     clickable: true,
   },
 });
-var swiperB = new Swiper(".swiperB", {
+const swiperB = new Swiper(".swiperB", {
   slidesPerView: 3,
   spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination-B",
-    clickable: true,
+  freeMode: Boolean,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
