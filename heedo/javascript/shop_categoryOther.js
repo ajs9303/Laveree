d1e52f4c5524cd05952e84f4083675130607ea34
Ted <tedlee1444@gmail.com>
2025-09-30 16:46:19 +0900

    16:46

---
+++ heedo/javascript/shop_categoryOther.js
@@ -0,0 +1,10 @@
+import { productLists } from "./shop_categoryAllProductsLists";
+import { categoryAll } from "./shop_categoryAllProductsLists";
+
+const All = document.querySelector("#All");
+const Detergent = document.querySelector("#Detergent");
+const Remover = document.querySelector("#Remover");
+const Gift = document.querySelector("#Gift");
+const Acc = document.querySelector("#Acc");
+
+Detergent.addEventListener("click", () => {});
