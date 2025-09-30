52126d238a442882591c084f1b58b9b47baa0fe1
tkddnjs0630 <tkddnjs063089@gmail.com>
2025-09-30 16:53:30 +0900

    sangwon

--- sangwon/js/index.js
+++ sangwon/js/index.js
@@ -1,5 +1,17 @@
 // hero
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
+var swiperA = new Swiper(".swiperA", {
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination-A",
+    clickable: true,
+  },
+});
+var swiperB = new Swiper(".swiperB", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination-B",
+    clickable: true,
+  },
 });
