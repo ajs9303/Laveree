f66d696378133818be5c45ca0d2120665f09426f
Ted <tedlee1444@gmail.com>
2025-09-30 11:07:17 +0900

    25.09.30 11:07

---
+++ heedo/javascript/shop_index.js
@@ -0,0 +1,143 @@
+const productLists = [
+  {
+    name: "STAIN PEMOVER",
+    price: "36,000원",
+    details: "얼룩제거제 295ML",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/stain_remover.jpg",
+  },
+  {
+    name: "DETERGENT-FOREST",
+    price: "45,000원",
+    details: "세탁세제 900ML",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/detergent_forest.jpg",
+  },
+  {
+    name: "DETERGENT-WIND",
+    price: "45,000원",
+    details: "세탁세제 900ML",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/detergent_wind.jpg",
+  },
+  {
+    name: "[GIFT] DETERGENT",
+    price: "50,000원",
+    details: "선물포장 세탁세제 900ML",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/detergent_gift.jpg",
+  },
+  {
+    name: "[GIFT] STAIN REMOVER",
+    price: "41,000원",
+    details: "선물포장 얼룩제거제 295ML",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/stain_remover_gift.jpg",
+  },
+  {
+    name: "WASHING DUO",
+    price: "95,000원",
+    details: "2종 선물세트",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/washing_duo.jpg",
+  },
+  {
+    name: "ESSENTIAL DUO",
+    price: "86,000원",
+    details: "2종 선물세트",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/essential_duo.jpg",
+  },
+  {
+    name: "ULTIMATE TRIO",
+    price: "131,000원",
+    details: "3종 선물세트",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/ultimate_trio.jpg",
+  },
+  {
+    name: "[TRAVEL] REMOVER STICKER POUCH",
+    price: "19,000원",
+    details: "여행용 얼룩제거제 스티커 파우치",
+    imgSrc:
+      "/Laveree-main/heedo/shop_images/products/remover_sticker_pouch_travel.jpg",
+  },
+  {
+    name: "[TRAVEL] ESSENTIAL DUO",
+    price: "33,000원",
+    details: "여행용 2종 선물세트",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/essential_duo_travel.jpg",
+  },
+  {
+    name: "[TRAVEL] STAIN REMOVER",
+    price: "15,000원",
+    details: "얼룩제거제 59ML",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/stain_remover_travel.jpg",
+  },
+  {
+    name: "[TRAVEL] DETERGENT-FOREST",
+    price: "14,000원",
+    details: "세탁세제 59ML",
+    imgSrc:
+      "/Laveree-main/heedo/shop_images/products/detergent_forest_travel.jpg",
+  },
+  {
+    name: "[TRAVEL] DETERGENT-WIND",
+    price: "14,000원",
+    details: "세탁세제 59ML",
+    imgSrc:
+      "/Laveree-main/heedo/shop_images/products/detergent_wind_travel.jpg",
+  },
+  {
+    name: "ECO BAG",
+    price: "12,900원",
+    details: "에코백",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/eco_bag.jpg",
+  },
+  {
+    name: "LAVEREE X BLOOMINGTALE",
+    price: "29,000원",
+    details: "미니뿌 키링",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/Laveree_bloomingtale.jpg",
+  },
+  {
+    name: "FELT BASKET",
+    price: "9,900원",
+    details: "펠트 바스켓",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/felt_basket.jpg",
+  },
+  {
+    name: "SCRUNCHINE",
+    price: "15,900원",
+    details: "헤어 스크런치",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/scrunchie.jpg",
+  },
+  {
+    name: "PUMP",
+    price: "3,000원",
+    details: "세제용 펌프",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/pump.jpg",
+  },
+  {
+    name: "GUN SPRAY",
+    price: "1,000원",
+    details: "건 스프레이",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/gun_spray.jpg",
+  },
+  {
+    name: "MEASURING CUP",
+    price: "1,000원",
+    details: "세제용 계량컵",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/measuring_cup.jpg",
+  },
+  {
+    name: "SHOPPING BAG",
+    price: "1,000원",
+    details: "쇼핑백 (XS)",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/shopping_bag(XS).jpg",
+  },
+  {
+    name: "SHOPPING BAG",
+    price: "1,500원",
+    details: "쇼핑백 (S)",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/shopping_bag(S).jpg",
+  },
+  {
+    name: "SHOPPING BAG",
+    price: "2,000원",
+    details: "쇼핑백 (M)",
+    imgSrc: "/Laveree-main/heedo/shop_images/products/shopping_bag(M).jpg",
+  },
+];
