e64679fcdcbf8016c400fb552e6451acd2d48f06
tkddnjs0630 <tkddnjs063089@gmail.com>
2025-10-01 14:38:57 +0900

    sangwon

--- sangwon/js/index.js
+++ sangwon/js/index.js
@@ -1,24 +1,3 @@
-const title = document.querySelectorAll(".hiddentitle");
-const BRAND = document.querySelector("#BRAND");
-const SHOP = document.querySelector("#SHOP");
-const MAGAZINE = document.querySelector("#MAGAZINE");
-const COMMUNITY = document.querySelector("#COMMUNITY");
-const STORE = document.querySelector("#STORE");
-
-title.forEach((title) => {
-  BRAND.addEventListener("mouseover", () => {
-    title.classList.remove("hiddentitle");
-  });
-});
-
-// BRAND.addEventListener("mouseover", () => {
-//   BRAND.classList.remove("hiddentitle");
-//   SHOP.classList.remove("hiddentitle");
-//   BRAND.classList.remove("hiddentitle");
-//   BRAND.classList.remove("hiddentitle");
-//   BRAND.classList.remove("hiddentitle");
-// });
-
 // hero
 const swiperA = new Swiper(".swiperA", {
   spaceBetween: 0,
