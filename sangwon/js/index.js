310b548d2f4013cb916aaaac8d10025c137d4add
tkddnjs0630 <tkddnjs063089@gmail.com>
2025-10-02 17:04:22 +0900

    10021704

--- sangwon/js/index.js
+++ sangwon/js/index.js
@@ -15,6 +15,19 @@ const swiperB = new Swiper(".swiperB", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    // 768px 이상일 때 적용될 설정 (기본 설정이므로 생략 가능하나 명시적으로 정의)
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // 767.98px 이하일 때 적용될 설정 (최대 767px까지 적용)
+    // 모바일 뷰에서는 0px부터 이 설정이 적용됩니다.
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10, // 모바일에서 슬라이드 간격을 줄일 수 있습니다. (선택 사항)
+    },
+  },
 });
 
 //phone
